e22652c099d221e2668054076194ef4f8f41a88e
Esteban <corlo.esteban@gmail.com>
2019-11-08 16:20:57 -0600

    Initial Commit

---
+++ CG_demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG_demo
+//
+//  Created by Esteban Cortes Lozano on 08/11/19.
+//  Copyright © 2019 Esteban Cortes Lozano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
